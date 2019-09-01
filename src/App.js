89f9fa9e3ddfb5ea39971ff6344323f8f8a01b9d
Gustavo Sanchez <antero105@gmail.com>
2019-09-01 13:31:46 -0400

    Using class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Menu from './components/Menu';
+import {Provider} from 'react-redux';
+import store from './store';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Menu/>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Provider>
+    )
+  }
 }
+
 
 export default App;
