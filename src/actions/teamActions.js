41e5cd8e517ade15b9d426ffeb61973cd3248b12
Gustavo Sanchez <antero105@gmail.com>
2019-09-01 19:48:59 -0400

    Adding fetch team and roster action

--- src/actions/teamActions.js
+++ src/actions/teamActions.js
@@ -1,11 +1,29 @@
-import {FETCH_TEAMS} from './types';
-import {mlbService} from '../services';
+import { FETCH_TEAMS, FETCH_TEAM, FETCH_ROSTER} from './types';
+import { mlbService } from '../services';
 
 export const fetchTeams = ()  => dispatch => {
     mlbService.get('teams').then((teams) => {
         dispatch({
             type: FETCH_TEAMS,
             payload: teams.data,
+        });
+    });
+}
+
+export const fetchTeam = (id) => dispatch => {
+    mlbService.get(`teams/${id}`).then((team) => {
+        dispatch({
+            type: FETCH_TEAM,
+            payload: team.data
+        });
+    });
+}
+
+export const fetchRoster = (id) => dispatch => {
+    mlbService.get(`teams/${id}/roster`).then((team) => {
+        dispatch({
+            type: FETCH_ROSTER,
+            payload: team.data
         });
     });
 }
