e86d8e2685e7659d96f99e320ba8cc489982a748
Gustavo Sanchez <antero105@gmail.com>
2019-09-02 14:58:03 -0400

    Adding fetch team  games and improving code

--- src/actions/teamActions.js
+++ src/actions/teamActions.js
@@ -1,29 +1,46 @@
-import { FETCH_TEAMS, FETCH_TEAM, FETCH_ROSTER} from './types';
+import { FETCH_TEAMS, FETCH_TEAM, FETCH_ROSTER, FETCH_GAMES } from './types';
 import { mlbService } from '../services';
 
 export const fetchTeams = ()  => dispatch => {
-    mlbService.get('teams').then((teams) => {
+    mlbService.get('teams').then((response) => {
         dispatch({
             type: FETCH_TEAMS,
-            payload: teams.data,
+            payload: response.data,
         });
     });
-}
+};
 
 export const fetchTeam = (id) => dispatch => {
-    mlbService.get(`teams/${id}`).then((team) => {
+    mlbService.get(`teams/${id}`).then((response) => {
         dispatch({
             type: FETCH_TEAM,
-            payload: team.data
+            payload: response.data
         });
     });
-}
+};
 
 export const fetchRoster = (id) => dispatch => {
-    mlbService.get(`teams/${id}/roster`).then((roster) => {
+    mlbService.get(`teams/${id}/roster`).then((response) => {
         dispatch({
             type: FETCH_ROSTER,
-            payload: roster.data
+            payload: response.data
         });
     });
-}
+};
+
+
+export const fetchTeamGamesData = (sportId, teamId, startDate = '01/01/2019', endDate = '12/31/2019') => dispatch => {
+    mlbService.get('schedule', {
+        params: {
+            sportId,
+            teamId,
+            startDate,
+            endDate
+        }
+    }).then((response) => {
+        dispatch({
+            type: FETCH_GAMES,
+            payload: response.data
+        });
+    });
+};
